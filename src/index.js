0807285fa30f099052e7b31353eb2a0b99049951
Volodymyr Pasichnyi <vlpasnj33@gmail.com>
2023-02-06 11:59:23 +0200

    up

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
--- src/redux/contactsSlice.js
+++
@@ -1,8 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-
-export const contactsSlice = createSlice({
-    name: 'contacts',
-    initialState: [],
-    
-})
